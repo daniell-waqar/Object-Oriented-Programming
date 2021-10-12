dd6b5c24487bcc336d4e9265e3da6d87358d8814
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-10-12 23:30:42 +0500

    initial commit

---
+++ Lab-work/TASK-1/Q1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    cout<<"Size of int : "<<sizeof(int) << " bytes\n";
+
+    cout<<"Size of char : "<<sizeof(char) << " byte\n";
+
+    cout<<"Size of float : "<<sizeof(float) << " bytes\n";
+
+    cout<<"Size of long : "<<sizeof(long) << " bytes\n";
+
+    cout<<"Size of double : "<<sizeof(double) << " bytes\n";
+
+    return 0;
+}
