f658bafd45a5ac44bbb58973a9ec9dc95015f3f4
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-10-31 18:37:51 +0500

    initial commit

---
+++ Lab-Assignments/Assignment-01/if else/Q1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout << "Enter the number : ";
+    cin >> n;
+
+    if(n > 0)
+    {
+        cout << n << " is positive number" << endl;
+    }
+
+    else
+    {
+        cout << n << " is negative number" << endl;
+    }
+
+    return 0;
+
+}
---
+++ Lab-Assignments/Assignment-01/if else/Q4.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1,num2;
+
+    cout << "Enter first number : ";
+    cin >> num1;
+
+    cout << "Enter second number : ";
+    cin >> num2;
+
+    if(num1 > num2)
+    {
+        cout << num1 << " is greater" << endl;
+    }
+
+    else
+    {
+        cout << num2 << " is greater" << endl;
+    }
+
+    return 0;
+
+}
