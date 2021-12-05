836c5e40a08df0c4c7f3ed7aa8886071e822d542
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-12-05 22:21:57 +0500

    -

---
+++ cpp-files/reference_variable.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+  int x = 10;
+
+  // ref is a reference to x.
+  int& ref = x;
+
+  // Value of x is now changed to 20
+  ref = 20;
+  cout << "x = " << x << endl ;
+
+  // Value of x is now changed to 30
+  x = 30;
+
+
+  cout << "ref = " << ref << endl ;
+
+  cout << "The address of variable x is " << &x << endl;
+  cout << "The address of variable ref is " << &ref << endl;
+
+  
+
+  return 0;
+}
