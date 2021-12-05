15ce43bf1c110ad815405768f5cb96150651af62
daniell-waqar <p200750@pwr.nu.edu.pk>
2021-12-05 22:18:20 +0500

    -

---
+++ cpp-files/Public Access Specifier.cpp
@@ -0,0 +1,31 @@
+// C++ program to demonstrate private access modifier
+
+#include <iostream> 
+using namespace std;
+
+class Circle
+{   
+public:
+    double r;
+    double calculate_area()
+    {
+        return 3.14*r*r;
+    }
+
+
+
+};
+
+int main()
+{
+    Circle obj;
+
+    obj.r = 5.5;    // accessing public data member outside class
+
+    cout << "Radius of Circle : " << obj.r << endl;
+
+    cout << "Area of Circle : " << obj.calculate_area() << endl;
+
+    return 0;
+}
+
